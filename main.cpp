276f11db09c5b43ff8f539ee2cfac80872c5123f
Chen98 <osmar.chen98@gmail.com>
2017-09-29 03:50:58 -0300

    Comments at the codes

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(void){
 	cin >> nroCidades >> nroArestas;
 
 	/*
-        Aloca dinamicamente um vetor "e" de tamanho "nroCidades" e, em seguida, faz a leitura dos inteiros das "nroCidades" proximas linhas,
+        Aloca dinamicamente um vetor "e" de tamanho "nroCidades" e, em seguida, faz a leitura dos inteiros das proximas "nroCidades" linhas,
         salvando-os sequencialmente no vetor "e" para guardar a quantidade de egressos em cada cidade.
 	*/
 	int *e = new int[nroCidades];
@@ -22,7 +22,9 @@ int main(void){
 	double **matrizAdj = declaraMatriz(nroCidades);
 
 	/*
-        Declara as variaveis "cidade1", "cidade2" e "peso"
+        Declara as variaveis "cidade1", "cidade2" e "peso" e, em seguida, faz a leitura das proximas "nroArestas" linhas,
+        recebendo dois numeros inteiros e um numero real em cada linha, sendo os inteiros, correspondentes as cidades, os indices para a insercao
+        do real "peso" na matriz de adjacencia.
 	*/
 	int cidade1, cidade2;
 	double peso;
@@ -31,14 +33,26 @@ int main(void){
 		insereAresta(matrizAdj, cidade1, cidade2, peso);
 	}
 
+	/*
+        Declara uma matriz "output", semelhante a matriz de adjacencia "matrizAdj".
+        A matriz "matrizAdj" armazena e permanece com os valores de entrada,
+        enquanto a matriz "output", que possui os mesmos valores de "matrizAdj", tem seus valores
+        modificados a fim de gerar as saidas correspondentes aos criterios exigidos pelo trabalho.
+	*/
 	int** output = new int*[nroCidades];
 	for(i=0;i<nroCidades;i++)
 		output[i] = new int[nroCidades];
 
-
+    /*
+        Imprime as saidas correspondentes aos criterios do trabalho.
+    */
 	cout << criterioUm(matrizAdj, nroCidades, e, output) << endl;
 	cout << criterioDois(matrizAdj, nroCidades, e, output) << endl;
 
+	/*
+        Libera as memorias alocadas anteriormente, para a resolucao do trabalho.
+	*/
 	free(e);
 	destroiMatriz(nroCidades, matrizAdj);
+	//  Falta dar free no "output"?
 }
